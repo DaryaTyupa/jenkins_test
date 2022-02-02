af5ac5f587e3178ab2d64eee80197b6e45e661ef
Darya Tyupa <91557639+DaryaTyupa@users.noreply.github.com>
2022-02-02 16:41:10 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -4,6 +4,6 @@ from selenium.webdriver import Firefox
 
 @pytest.fixture(scope='session')
 def driver():
-    driver = Firefox()
+    driver = Firefox('/home/darya/virtualenvs/selenium/lib/python3.7/site-packages/selenium/webdriver/firefox')
     yield driver
     driver.quit()
